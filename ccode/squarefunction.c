16500ca239f0eb266de9d1cb336b1eacf93e591a
Cynthia Wade <31551024+CynthiaW19@users.noreply.github.com>
2018-01-11 13:24:17 -0800

    Update squarefunction.c

--- ccode/squarefunction.c
+++ ccode/squarefunction.c
@@ -1,20 +1,20 @@
-//sqaurefunction.c
+//squarefunction.c
 
 #include<stdio.h>
-int sqaure ( int v );
+int square ( int v );
 
 int main( )
 {
 	x, n ;
-	printf ("\Enter some number for the finding sqaure \n");
+	printf ("\Enter some number for the finding square \n");
 	scanf ("%d", &n);
 	x = sqaure ( n );
-	printf ("nSqaure of the given number %d is %d",n,x);
+	printf ("nSquare of the given number %d is %d",n,x);
 	printf("\n\n");
 	return 0;
 }
 
-int sqaure ( int v ) //function definition
+int square ( int v ) //function definition
 {	
 	int p;
 	p = v * v ;
